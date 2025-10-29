1b6f166c9603b93fa8ce447d295bbffda477dba7
mahfuzdotnet <mahfuzfsl@gmail.com>
2025-10-29 23:10:59 +0600

    Updated comment

--- config.ts
+++ config.ts
@@ -1,4 +1,5 @@
-// src/config.ts
+// src/config.ts api base URL 
 export const API_BASE = import.meta.env.VITE_API_BASE ?? "/api";
 export const STRIPE_PK = import.meta.env.VITE_STRIPE_PUBLIC_KEY ?? "";
 export const PAYPAL_ID = import.meta.env.VITE_PAYPAL_CLIENT_ID ?? "";
+export const PAYPAL_ID_test = import.meta.env.VITE_PAYPAL_CLIENT_ID ?? "";
