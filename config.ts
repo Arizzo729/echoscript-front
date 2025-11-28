fe8538923796d1e7cf65cc2667ba94b6482a1963
mahfuzdotnet <mahfuzfsl@gmail.com>
2025-11-29 05:12:06 +0600

    commit for new feature

--- config.ts
+++ config.ts
@@ -1,5 +1,6 @@
 // src/config.ts api base URL 
-export const API_BASE = import.meta.env.VITE_API_BASE ?? "/api";
+export const API_BASE = import.meta.env.VITE_API_BASE ?? ""; 
+// export const API_BASE = import.meta.env.VITE_API_BASE ?? "/api";
 export const STRIPE_PK = import.meta.env.VITE_STRIPE_PUBLIC_KEY ?? "";
 export const PAYPAL_ID = import.meta.env.VITE_PAYPAL_CLIENT_ID ?? "";
 export const PAYPAL_ID_test = import.meta.env.VITE_PAYPAL_CLIENT_ID ?? "";
